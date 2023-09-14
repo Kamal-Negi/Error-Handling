961c43ae55ab9c3bc5723739d5bd5c845dcb9b0e
Kamal-Negi <kamal.negi@getmyparking.com>
2023-09-14 17:36:50 +0530

    Initial Commit

---
+++ Error_Handling/Error_Handling.h
@@ -0,0 +1,18 @@
+//
+//  Error_Handling.h
+//  Error_Handling
+//
+//  Created by Kamal Negi on 14/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Error_Handling.
+FOUNDATION_EXPORT double Error_HandlingVersionNumber;
+
+//! Project version string for Error_Handling.
+FOUNDATION_EXPORT const unsigned char Error_HandlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Error_Handling/PublicHeader.h>
+
+
